a23009a516d1ae63632a2f9ff78c5a8bc1d9ab64
willgner <willgnersf@alu.ufc.br>
2024-12-09 16:08:32 -0300

    Criando template Topic

---
+++ src/Topic.java
@@ -0,0 +1,31 @@
+public class Topic {
+
+    public Topic(int capacidade, int qtdPrioritatios) {
+    }
+
+    public int getNumeroAssentosPrioritarios() {
+        return 0;
+    }
+    public int getNumeroAssentosNormais() {
+        return 0;
+    }
+
+    public Passageiro getPassageiroAssentoNormal(int lugar) {
+        return null;
+    }
+
+    public Passageiro getPassageiroAssentoPrioritario(int lugar) {
+        return null;
+    }
+
+    public int getVagas() {
+        return -1;
+    }
+
+    public boolean subir(Passageiro passageiro) {
+        return false;
+    }
+    public boolean descer(String nome) {
+        return true;
+    }
+}
