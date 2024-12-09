829a1f7c3782266a9843407efa08e517e14502db
willgner <willgnersf@alu.ufc.br>
2024-12-09 16:07:50 -0300

    Criando template Passageiro

---
+++ src/Passageiro.java
@@ -0,0 +1,17 @@
+public class Passageiro {
+
+    public Passageiro(String nome, int idade) {
+
+    }
+    public boolean ePrioritario() {
+        return true;
+    }
+
+    String getNome() {
+        return null;
+    }
+
+    public int getIdade() {
+        return 0;
+    }
+}
